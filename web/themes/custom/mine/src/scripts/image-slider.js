774ddad8c33f186b99c15a3cbf17b93a7f4f1d3c
yacineredjala <yacine.redjala@dms.be>
2023-12-12 20:15:36 +0100

    Text & image slider & setup scroll animation + fixedmenu

--- web/themes/custom/mine/src/scripts/image-slider.js
+++ web/themes/custom/mine/src/scripts/image-slider.js
@@ -2,16 +2,25 @@ import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
 
 (function ($, Drupal) {
-
+  
   'use strict';
-
+  
   Drupal.behaviors.image_slider = {
     attach: function (context, settings) {
-      let sliders = $('.paragraph--type--image-slider.paragraph--view-mode--default .field--name-field-media')
+      gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,TextPlugin);
+      let defaultSliders = $('.paragraph--type--image-slider.paragraph--view-mode--default .field--name-field-media');
+      let scrollSliders = $('.paragraph--type--image-slider.paragraph--view-mode--scroll_animation .field--name-field-media');
 
-      $(once('swiper', sliders)).each(function (index, item) {
+      $(once('swiper', defaultSliders)).each(function (index, item) {
         $(item).addClass('swiper');
         $(item).find('.field__item').addClass('swiper-slide');
         $(item).children().wrapAll('<div class="swiper-wrapper"></div>');
@@ -29,6 +38,10 @@ import 'swiper/css/navigation';
           }
         });
       });
+
+      // write logic for scorll trigger
+
+
     }
   };
 
